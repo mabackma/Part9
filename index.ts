8d1aa12d31bc2140079468e8adf5fd5cc33e06eb
Marc Bäckman <marc.backman@edu.lapinamk.fi>
2023-10-05 04:28:22 +0300

    exercises

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import calculateBmi from './bmiCalculator';
+import calculateExercises from './exerciseCalculator';
 import express from 'express';
 const app = express();
+import bodyParser from 'body-parser';
+
+// Parse JSON data when posting exercise data
+app.use(bodyParser.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -27,6 +32,25 @@ app.get('/bmi', (req, res) => {
 
   const jsonString = `{\n  weight: ${weight},\n  height: ${height},\n  bmi: "${bmi}"\n}`;
   res.type('json').send(jsonString);
+});
+
+app.post('/exercises', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body;
+
+  if (!daily_exercises || !target) {
+    res.status(400).json({ error: 'parameters missing' });
+    return;
+  }
+
+  if (!Array.isArray(daily_exercises) ||  !daily_exercises.every((value) => typeof value === "number") || typeof target !== "number") {
+    res.status(400).json({ error: 'malformatted parameters' });
+    return;
+  }
+  
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const exerciseResult = calculateExercises(daily_exercises, target);
+  res.json(exerciseResult);
 });
 
 const PORT = 3003;
